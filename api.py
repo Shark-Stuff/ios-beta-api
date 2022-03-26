bae32c234c96be20d42d9438b291e673d54a29b1
sharklatan <element.ro@gmail.com>
2022-03-26 00:46:43 -0600

    Update api.py

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
 import uvicorn
 
 if __name__ == '__main__':
-    uvicorn.run(app='scraper:app', workers=2)
+    uvicorn.run(app='scraper:app', host="127.0.0.1", port=8000, workers=2, log_level="info")
